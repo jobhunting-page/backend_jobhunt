5b33218bd75650ca398e704c9df8334d612c7f13
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-04-29 17:38:54 +0900

    complet chat gpt
    chat gpt 메소드 문자열 파싱부분 해결

--- src/main/java/com/example/jobhuntwithjpa/Service/ChatGPTService.java
+++ src/main/java/com/example/jobhuntwithjpa/Service/ChatGPTService.java
@@ -1,12 +1,16 @@
 package com.example.jobhuntwithjpa.Service;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.*;
 
 @Service
 public class ChatGPTService {
@@ -14,12 +18,11 @@ public class ChatGPTService {
     private String OPENAI_API_KEY;
     private final String OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
 
+    private final RestTemplate restTemplate = new RestTemplate();
+
     public ChatGPTService(@Value("${openai.api-key}") String secret){
         this.OPENAI_API_KEY = secret;
     }
-
-
-    private RestTemplate restTemplate = new RestTemplate();
 
     public String generateText(String prompt) {
 
@@ -28,13 +31,43 @@ public class ChatGPTService {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setBearerAuth(OPENAI_API_KEY);
 
-        String requestBody = "{\"model\": \"gpt-3.5-turbo\",\"messages\": [{\"role\": \"user\",\"content\": \""+ prompt + "\"}],\"temperature\": 0.7}";
+        /*Map<String, Object> requestBody = new HashMap<>();
+        Map<String, Object> messageBody = new HashMap<>();
+        messageBody.put("role", "user");
+        messageBody.put("content", prompt);
 
+        requestBody.put("model","gpt-3.5-turbo");
+        requestBody.put("messages", messageBody);
+        requestBody.put("temperature", 0.7);*/
 
-        HttpEntity<String> request = new HttpEntity<String>(requestBody, headers);
-        ResponseEntity<String> response = restTemplate.postForEntity(OPENAI_API_URL, request, String.class);
+        //String requestBody = "{\"model\": \"gpt-3.5-turbo\",\"messages\": [{\"role\": \"user\",\"content\": \""+ prompt + "\"}],\"temperature\": 0.7}";
 
-        return response.getBody();
+        List<Map<String, Object>> messageList = new ArrayList<>();
+        Map<String, Object> messageBody = new HashMap<>();
+        messageBody.put("role", "user");
+        messageBody.put("content", prompt);
+        messageList.add(messageBody);
+
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("model", "gpt-3.5-turbo");
+        requestBody.put("messages", messageList);
+        requestBody.put("temperature", 0.7);
+
+        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(OPENAI_API_URL, HttpMethod.POST, requestEntity, String.class);
+        String content = "";
+
+        if (response.getStatusCode() == HttpStatus.OK) {
+            JsonObject jsonObject = JsonParser.parseString(response.getBody()).getAsJsonObject();
+            System.out.println(jsonObject.get("choices").getAsJsonArray().get(0).getAsJsonObject().get("message").getAsJsonObject().get("content"));
+            content = jsonObject.get("choices").getAsJsonArray().get(0).getAsJsonObject().get("message").getAsJsonObject().get("content").toString();
+
+        }
+        /*RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Map> response = restTemplate.postForEntity(OPENAI_API_URL, requestEntity, Map.class);*/
+
+        return content;
     }
 }
 
