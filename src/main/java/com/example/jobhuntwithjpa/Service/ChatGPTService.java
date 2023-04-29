83e99d7263e5d4281a55a433694b950b4e04a34b
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-04-29 16:29:03 +0900

    chat gpt
    chat gpt api key 보안성 강화

--- src/main/java/com/example/jobhuntwithjpa/Service/ChatGPTService.java
+++ src/main/java/com/example/jobhuntwithjpa/Service/ChatGPTService.java
@@ -1,5 +1,6 @@
 package com.example.jobhuntwithjpa.Service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -10,18 +11,24 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class ChatGPTService {
 
-    private final String OPENAI_API_KEY = "sk-";
+    private String OPENAI_API_KEY;
     private final String OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
+
+    public ChatGPTService(@Value("${openai.api-key}") String secret){
+        this.OPENAI_API_KEY = secret;
+    }
 
 
     private RestTemplate restTemplate = new RestTemplate();
 
     public String generateText(String prompt) {
+
+        //System.out.println(OPENAI_API_KEY);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setBearerAuth(OPENAI_API_KEY);
 
-        String requestBody = "{\"model\": \"gpt-3.5-turbo\",\"messages\": [{\"role\": \"user\",\"content\": \""+ prompt + "?\"}],\"temperature\": 0.7}";
+        String requestBody = "{\"model\": \"gpt-3.5-turbo\",\"messages\": [{\"role\": \"user\",\"content\": \""+ prompt + "\"}],\"temperature\": 0.7}";
 
 
         HttpEntity<String> request = new HttpEntity<String>(requestBody, headers);
